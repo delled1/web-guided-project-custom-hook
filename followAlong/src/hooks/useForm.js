c69f4d0f2ad8e2337c2206460fb3a13c3c8a4f97
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-07 17:40:43 -0800

    lecture work-in-progress

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 
 export const useForm = () => {
   const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleChanges = e => {
     setFirstName(e.target.value);
   };
 
+  const handleLastNameChanges = e => {
+    setLastName(e.target.value);
+  }
+
   const handleSubmit = e => {
     e.preventDefault();
-    alert(firstName);
+    alert(firstName, lastName);
   };
 
   const clearForm = e => {
     e.preventDefault();
     setFirstName("");
+    setLastName("");
   };
 
   return [firstName, handleChanges, handleSubmit, clearForm];
