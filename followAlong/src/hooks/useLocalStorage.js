99bc8adcf55e939a9d8d102e90478c9d5a2de8f5
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-07 18:05:54 -0800

    lecture work-in-progress

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -1,1 +1,9 @@
 import {useState} from "react";
+
+// Requirements:
+// same format as useState from the component's perspective
+// If the value is already saved in localStorage (e.g., form already started), use those values
+// Otherwise, initialize with the initialValue passed in
+// Then, every time we update state, also update localStorage to keep it in sync
+
+export const useLocalStorage = (key, initialValue)
